a4fd25fdae42ebcfe16e9098cd4da5601eeefd7a
Min <zinminphyo@neversitup.com>
2023-03-25 15:06:54 +0630

    Implemented Pod File.

---
+++ AiOESports/Pods/Target Support Files/Pods-AiOESports-AiOESportsUITests/Pods-AiOESports-AiOESportsUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_AiOESports_AiOESportsUITests : NSObject
+@end
+@implementation PodsDummy_Pods_AiOESports_AiOESportsUITests
+@end
---
+++ AiOESports/Pods/Target Support Files/Pods-AiOESportsTests/Pods-AiOESportsTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_AiOESportsTests : NSObject
+@end
+@implementation PodsDummy_Pods_AiOESportsTests
+@end
