4cad8e32c644eff25a2d62d8ddc21804d7c71639
Zin Min Phyo <zinminphyo@codigo.co>
2023-11-20 00:33:22 +0630

    Implemented Content Scroll View for updated details view.

--- AiOESports/AiOESports/Modules/Detail/controller/DetailController.swift
+++ AiOESports/AiOESports/Modules/Detail/controller/DetailController.swift
@@ -9,14 +9,14 @@ import UIKit
 
 class DetailController: UIViewController {
     
-    
+    @IBOutlet private(set) var contentScrollView: UIScrollView!
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-        
+        configureContentScrollView()
         
         
     }
@@ -24,6 +24,15 @@ class DetailController: UIViewController {
     @IBAction
     private func didTapBackBtn(_ sender: UIButton) {
         navigationController?.popViewController(animated: true)
+    }
+    
+    private func configureContentScrollView() {
+        guard let vc = AchivementModule.createModule(achivementLists: []) else {return}
+        vc.view.frame = CGRect(x: 0, y: 0, width: view.frame.width, height: contentScrollView.frame.height)
+        vc.willMove(toParent: self)
+        contentScrollView.addSubview(vc.view)
+        vc.didMove(toParent: self)
+        contentScrollView.contentSize = CGSize(width: view.frame.width, height: contentScrollView.frame.height)
     }
     
 
