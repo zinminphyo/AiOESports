13978f79c76c531681552ffc006fc3f1c8237c57
Zin Min Phyo <zinminphyo@codigo.co>
2023-11-29 15:43:06 +0630

    Implemented Rating Count view.

---
+++ AiOESports/AiOESports/Modules/Vote/view/RatingView/RatingView.swift
@@ -0,0 +1,51 @@
+//
+//  RatingView.swift
+//  AiOESports
+//
+//  Created by Zin Min Phyo on 29/11/2023.
+//
+
+import UIKit
+
+class RatingView: UIControl, NibLoadable {
+    
+    @IBOutlet private(set) var starCountLabel: UILabel!
+    @IBOutlet private(set) var oneStarImageView: UIImageView!
+    @IBOutlet private(set) var twoStarImageView: UIImageView!
+    @IBOutlet private(set) var threeStarImageView: UIImageView!
+    @IBOutlet private(set) var fourStarImageView: UIImageView!
+    @IBOutlet private(set) var fiveStarImageView: UIImageView!
+    
+    var count: Int = 0 {
+        didSet {
+            updateStars()
+        }
+    }
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        loadedByNib()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        loadedByNib()
+    }
+    
+    @IBAction
+    private func didTapRating(_ sender: UIButton) {
+        count = sender.tag
+    }
+    
+    private func updateStars() {
+        let selectedStarCount = 0..<count
+        let starImages: [UIImageView] = [
+            oneStarImageView, twoStarImageView, threeStarImageView, fourStarImageView, fiveStarImageView
+        ]
+        starImages.forEach{ $0.image = UIImage(named: "star-unfill") }
+        selectedStarCount.forEach{ starImages[$0].image = UIImage(named: "star-fill") }
+    }
+
+    
+    
+}
